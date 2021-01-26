b7acc17d70e5190a076a686e8f181524c3742dc2
maxonemillion <max.daniel@utah.edu>
2021-01-25 23:16:13 -0700

    updates

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 import { Navbar, Button, Card } from "react-bootstrap"
 import "./Main.css"
 import getColors from "get-image-colors";
+import hexToHsl from "hex-to-hsl";
+import hslToHex from "hsl-to-hex"
 
 
 
 const Main = () => {
     const [file, setFile] = useState(null)
     const [colorized, setColorized] = useState()
+    const [comp, setComp] = useState();
+    const [array, setArray] = useState();
 
     const hiddenInput = React.useRef(null);
 
@@ -16,12 +20,11 @@ const Main = () => {
     }
 
 
-
     const handleChange = (event) => {
-     
-            const uploadedFile = URL.createObjectURL(event.target?.files[0])
-            setFile(uploadedFile);
-   
+
+        const uploadedFile = URL.createObjectURL(event.target?.files[0])
+        setFile(uploadedFile);
+
         console.log(event)
 
     }
@@ -29,10 +32,32 @@ const Main = () => {
     const colorize = (e) => {
         getColors(file).then(colors => {
             setColorized(colors.map(color => color.hex()))
+            console.log(e)
+            colorized.map((hex) => {
+                const makeHSL = hexToHsl(hex);
+
+                const startingHue = makeHSL[0] - 1;
+                const newHue = (startingHue + 180) % 360;
+
+                const finalHex = hslToHex(newHue, makeHSL[1], makeHSL[2]);
+               
+                setComp(finalHex);
+            })
         })
-        console.log(e)
     }
 
+    console.log(comp)
+
+
+    const complimentary = (hex) => {
+        const makeHSL = hexToHsl(hex);
+
+        const startingHue = makeHSL[0] - 1;
+        const newHue = (startingHue + 180) % 360;
+
+        const finalHex = hslToHex(newHue, makeHSL[1], makeHSL[2]);
+        return finalHex
+    }
 
     return (
         <div>
@@ -44,22 +69,36 @@ const Main = () => {
             <Button variant="dark" id="uploadBtn" onClick={chooseFile}>upload</Button>
             <br></br>
             <input type="file" id="choose-file" ref={hiddenInput} onChange={handleChange}></input>
-            <Button variant="dark" id="goBtn" onClick={colorize}>go</Button>
+            <Button variant="dark" id="goBtn" onClick={() => { colorize(); }}>go</Button>
             <br></br>
             { colorized ? <h1 id="colorsExistHeader">colors</h1> : ""}
             {colorized?.map((color) => {
                 return (
                     <div>
-                        
+
                         <Card id="hexCard" style={{ backgroundColor: color }}>
                             <Card.Body>
-                            {color}
-                        </Card.Body>
+                                {color}
+                                {comp}
+                            </Card.Body>
                         </Card>
-                        
+
                     </div>
                 )
             })}
+            {/* {comp?.map((color) => {
+                return (
+                    <div>
+
+                        <Card id="hexCard" style={{ backgroundColor: color }}>
+                            <Card.Body>
+                                {color}
+                            </Card.Body>
+                        </Card>
+
+                    </div>
+                )
+            })} */}
 
         </div>
     )
