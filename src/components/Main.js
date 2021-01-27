7245adb37c3c6c95c38c3da341753ff2dfbf3542
maxonemillion <max.daniel@utah.edu>
2021-01-26 23:23:04 -0700

    updates

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar, Button, Card } from "react-bootstrap"
 import "./Main.css"
 import getColors from "get-image-colors";
 import hexToHsl from "hex-to-hsl";
 import hslToHex from "hsl-to-hex"
 
-
-
 const Main = () => {
     const [file, setFile] = useState(null)
     const [colorized, setColorized] = useState()
     const [comp, setComp] = useState();
     const [array, setArray] = useState();
+
+
+    useEffect(() => {
+        setComp(colorized?.map((hex) => {
+
+            const makeHSL = hexToHsl(hex);
+
+            const startingHue = makeHSL[0] - 1;
+            const newHue = (startingHue + 180) % 360;
+
+            const finalHex = hslToHex(newHue, makeHSL[1], makeHSL[2]);
+
+            return finalHex;
+        }))
+    }, [colorized])
 
     const hiddenInput = React.useRef(null);
 
@@ -29,38 +42,17 @@ const Main = () => {
 
     }
 
-    const colorize = (e) => {
+    const colorize = () => {
         getColors(file).then(colors => {
             setColorized(colors.map(color => color.hex()))
-            console.log(e)
-            colorized.map((hex) => {
-                const makeHSL = hexToHsl(hex);
-
-                const startingHue = makeHSL[0] - 1;
-                const newHue = (startingHue + 180) % 360;
-
-                const finalHex = hslToHex(newHue, makeHSL[1], makeHSL[2]);
-               
-                setComp(finalHex);
-            })
         })
     }
 
-    console.log(comp)
+    console.log(comp, colorized)
 
-
-    const complimentary = (hex) => {
-        const makeHSL = hexToHsl(hex);
-
-        const startingHue = makeHSL[0] - 1;
-        const newHue = (startingHue + 180) % 360;
-
-        const finalHex = hslToHex(newHue, makeHSL[1], makeHSL[2]);
-        return finalHex
-    }
 
     return (
-        <div>
+        <div style={{ backgroundImage: colorized ? `linear-gradient(${colorized[0]}, ${colorized[1]}, ${colorized[2]}, ${colorized[3]}, ${colorized[4]})` : ""}} className="mainDiv">
             {/* ${colorized[0]}, ${colorized[1]}, ${colorized[2]}, ${colorized[3]}, ${colorized[4]} */}
             <Navbar id="navbar">
                 <Navbar.Brand href="/">monkë</Navbar.Brand>
@@ -69,7 +61,7 @@ const Main = () => {
             <Button variant="dark" id="uploadBtn" onClick={chooseFile}>upload</Button>
             <br></br>
             <input type="file" id="choose-file" ref={hiddenInput} onChange={handleChange}></input>
-            <Button variant="dark" id="goBtn" onClick={() => { colorize(); }}>go</Button>
+            <Button variant="dark" id="goBtn" onClick={() => colorize()}>go</Button>
             <br></br>
             { colorized ? <h1 id="colorsExistHeader">colors</h1> : ""}
             {colorized?.map((color) => {
@@ -79,18 +71,19 @@ const Main = () => {
                         <Card id="hexCard" style={{ backgroundColor: color }}>
                             <Card.Body>
                                 {color}
-                                {comp}
+                               
                             </Card.Body>
                         </Card>
 
                     </div>
                 )
             })}
-            {/* {comp?.map((color) => {
+            { comp ? <h1 id="colorsExistHeader">complimentary</h1> : ""}
+            {comp?.map((color) => {
                 return (
                     <div>
 
-                        <Card id="hexCard" style={{ backgroundColor: color }}>
+                        <Card id="compCard" style={{ backgroundColor: color }}>
                             <Card.Body>
                                 {color}
                             </Card.Body>
@@ -98,7 +91,7 @@ const Main = () => {
 
                     </div>
                 )
-            })} */}
+            })}
 
         </div>
     )
